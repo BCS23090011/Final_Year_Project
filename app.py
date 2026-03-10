1edb5e361ebbf51a6bcb73739c9d5e3fa96fa63d
Kelvin Kee Kwong Yew <bcs23090011@student.uts.edu.my>
2026-03-10 08:11:18 +0800

    d

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -35,4 +36,5 @@ def about():
     return render_template('about.html', active_page='about')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
